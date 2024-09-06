80cb6a770343f5fa5983644edb957b3c1d309f35
Ngei-Zhu-Ming <163367868+Ngei-Zhu-Ming@users.noreply.github.com>
2024-09-06 08:13:31 +0800

    Update isipadu_tangki_air.py

--- isipadu_tangki_air.py
+++ isipadu_tangki_air.py
@@ -7,7 +7,7 @@ pi = 3.142
 def jejari_tinggi():
     r = float(input("Masukkan jejari tangki air:")) 
     h = float(input("Masukkan tinggi tangki air:"))
-    return (r, h) 
+    return (r,h) 
 
 def isipadu():
     (r,h) = jejari_tinggi()
